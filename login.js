313edf966b09ab4bf5de2fd7bff9b16756d4d9e5
Barros-15 <luiz.hbbarros03@gmail.com>
2024-11-22 19:04:45 -0300

    Update login.js
    Alteração de funções

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
-const signUpSubmitButton = document.querySelector('.sign-up-container button'); // Botão de "Criar Conta"
+const signUpSubmitButton = document.querySelector('.sign-up-container button'); 
 
 signUpButton.addEventListener('click', () => {
   container.classList.add("right-panel-active");
@@ -11,13 +11,13 @@ signInButton.addEventListener('click', () => {
   container.classList.remove("right-panel-active");
 });
 
-// Redireciona para a página principal quando "Criar Conta" é clicado
+
 signUpSubmitButton.addEventListener('click', () => {
-  // Adiciona a classe para a animação
+  
   container.classList.add("right-panel-active");
 
-  // Redireciona para a página index.html após 500ms (para esperar a animação)
+ 
   setTimeout(() => {
-    window.location.href = "index.html";  // Redireciona para o index.html
-  }, 500); // Aguarda 500ms para a animação de transição
+    window.location.href = "index.html";  
+  }, 500); 
 });
